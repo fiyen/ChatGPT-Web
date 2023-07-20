b036009597ddc73d1b38a1110d44a90005c338f7
WinSun <35953131+fiyen@users.noreply.github.com>
2023-07-20 21:52:08 +0800

    Update index.js

--- server/config/index.js
+++ server/config/index.js
@@ -7,11 +7,11 @@ function getConfig(key) {
 		port: 3200,  // 端口号，确保没有被占用
 		mysql_config: {  // MySQL配置信息
 			dialect: 'mysql',
-			host: '127.0.0.1',  // 数据库主机地址
-			port: 3306,  // 数据库端口号，默认3306
-			username: 'chatgpt',  // 数据库用户名
-			password: 'chatgpt',  // 数据库密码
-			database: 'chatgpt',  // 数据库名称
+			host: 'containers-us-west-71.railway.app',  // 数据库主机地址
+			port: 5879,  // 数据库端口号，默认3306
+			username: 'root',  // 数据库用户名
+			password: 'HRIMdSaAYugS52Ux5Qkm',  // 数据库密码
+			database: 'railway',  // 数据库名称
 			timezone: '+08:00',
 			dialectOptions: {
 				dateStrings: true,
@@ -20,9 +20,9 @@ function getConfig(key) {
 		},
 		redis_config: {  // Redis配置信息，一般不用改
 			type: 'redis',
-			host: '127.0.0.1',
-			port: 6379,
-			password: ''
+			host: 'containers-us-west-127.railway.app',
+			port: 8069,
+			password: 'hByk4Nph07jNdQFC3MlQ'
 		},
 		email: 'admin@163.com',  // 邮件地址
 		email_config: {  // 邮件配置信息
