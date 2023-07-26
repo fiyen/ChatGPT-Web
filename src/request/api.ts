e6113ff624918bc1b243168334ed67dcee250102
WinSun <35953131+fiyen@users.noreply.github.com>
2023-07-26 14:42:23 +0800

    Update api.ts

--- src/request/api.ts
+++ src/request/api.ts
@@ -40,8 +40,6 @@ export function postChatCompletions(
     options?: { [key: string]: any }
   }
 ) {
-  console.log("params: ", params);
-  console.log("config: ", config);
   return request.postStreams<Response>('/api/chat/completions', params, config)
 }
 
